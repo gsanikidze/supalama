e3fa0f2fa0580395ae7e330143fbd8857874023e
GSanikidze <sanikidze.guram@gmail.com>
2024-05-27 22:51:37 +0400

    [new] added shadcn + lucide icons

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-	import { Greet } from "$lib/wailsjs/go/main/App";
+	import { Input } from '$lib/components/ui/input';
+	import { Greet } from '$lib/wailsjs/go/main/App';
+	import { AArrowUp } from 'lucide-svelte';
 
-  let text = $state("")
+	let text = $state('');
 
-  Greet("Vite").then((res) => {
-    text = res
-  })
+	Greet('Vite').then((res) => {
+		text = res;
+	});
 </script>
 
 <h1 class="text-indigo-700 font-bold text-lg">{text}</h1>
+
+<Input placeholder="type message" class="mt-4" />
+
+<div class="p-2 bg-slate-400 w-fit rounded-sm mt-2">
+  <AArrowUp />
+</div>
