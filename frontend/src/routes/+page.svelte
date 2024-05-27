a22a56353fb765061d87db553496762c5fd9058a
GSanikidze <sanikidze.guram@gmail.com>
2024-05-27 22:15:37 +0400

    [init]

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Greet } from "$lib/wailsjs/go/main/App";
+
+  let text = $state("")
+
+  Greet("Vite").then((res) => {
+    text = res
+  })
+</script>
+
+<h1>{text}</h1>
